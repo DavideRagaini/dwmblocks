ceb4f6c26c23c985e2e443af8a19338379ab8b52
DavideRagaini <DavideRagaini@gmail.com>
2020-05-11 15:04:04 +0200

    basic Desktop customization

--- config.luke.h
+++ config.luke.h
@@ -1,29 +1,23 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	/* {"", "cat /tmp/recordingicon 2>/dev/null",	0,	9}, */
+	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11}, */
-	/* {"",	"pacpackages",	0,	8}, */
-	/* {"",	"dwm_spotify",	0,	5}, */
-	{"",	"dwm_mpc",	300,	11},
+	{"",	"pacpackages",	0,	8},
 	{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"torrent",	20,	7}, */
-	{"",	"cpu",		3,	13},
-	{"",	"memory",	10,	14},
+	{"",	"torrent",	20,	7},
+	/* {"",	"memory",	10,	14}, */
+	/* {"",	"cpu",		10,	13}, */
 	/* {"",	"moonphase",	18000,	5}, */
-	/* {"",	"weather",	18000,	5}, */
-	/* {"",	"mailbox",	180,	12}, */
-	{"",	"nettraf",	1,	16},
-	{"",	"videoOutput",	3600,	4},
-	/* {"",	"dwm_tlp",	3600,	4}, */
-	{"",	"sink",		3600,	12},
-	{"",	"volume",	60,	10},
-	/* {"",	"battery | tr \'\n\' \' \'",	5,	3}, */
-	/* {"",	"internet",	5,	4}, */
-	{"",	"uptime | awk '{print $3}' | sed 's/,//g'",	60,	1},
-	{"",	"clock",	1,	1},
-	/* {"",	"help-icon",	0,	15}, */
+	{"",	"weather",	18000,	5},
+	{"",	"mailbox",	180,	12},
+	/* {"",	"nettraf",	1,	16}, */
+	{"",	"volume",	0,	10},
+	{"",	"battery | tr \'\n\' \' \'",	5,	3},
+	{"",	"clock",	60,	1},
+	{"",	"internet",	5,	4},
+	{"",	"help-icon",	0,	15},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
